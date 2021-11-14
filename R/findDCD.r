5b274db13334c6c1426498b34d2cf281a4e1e7e2
AndresMCB <andresm12@gmail.com>
2021-11-14 13:27:07 +1030

    changes to description

--- R/findDCD.r
+++ R/findDCD.r
@@ -256,6 +256,9 @@ findDCD <- function(GeneExpression, z=NULL, pathCovariate =NULL
 
   CDinfer <- cbind(AMCBGeneUtils::changeGeneId(CDinfer[,1], from = "Ensembl.ID")[2:4]
                    ,CDinfer[,-1])
+  rename(CDinfer, p = p.val)
+
+
 
   data(CGC.driverNames)
   nCD <- length(CD)
--- Test DynamicCancerDriver.R
+++ Test DynamicCancerDriver.R
@@ -5,7 +5,7 @@
 #
 #
 
-#rm(list = ls())
+rm(list = ls())
 if (!requireNamespace("devtools", quietly = TRUE))
   install.packages("devtools")
 
@@ -13,26 +13,30 @@ if (!requireNamespace("tidyverse", quietly = TRUE))
   install.packages("tidyverse")
 
 if (!requireNamespace("AMCBGeneUtils", quietly = TRUE))
-devtools::install_github(repo = "AndresMCB/AMCBGeneUtils")
+  devtools::install_github(repo = "AndresMCB/AMCBGeneUtils")
 
 if (!requireNamespace("DynamicCancerDriver", quietly = TRUE))
   devtools::install_github(repo = "AndresMCB/DynamicCancerDriver")
 
-#library(DynamicCancerDriver)
+library(DynamicCancerDriver)
 library(tidyverse)
+
 
 #### ----- Load Single Cell Data ------ ####
 # pre-processed Single Cell data, GSE75688
 # Genes not expressed in a least 20% of the dataset were removed.
 # afterwards, only samples from tumor were kept
 
-data("GSE75688_TPM_tumor")
+data("GSE75688_TPM_tumor", package = "DynamicCancerDriver")
 
 #----- Find Dynamic Cancer Drivers, PPI top 40% -----
 DCD.HER2time_SC <- findDCD(GeneExpression = GSE75688_TPM_tumor
                            , pathCovariate = "HER2"
                            , PPItop = 0.4
                            , findEvent = TRUE)
+
+write.csv(DCD.HER2time_SC$res$CDinfer
+         , file =  "supplementary table 1 - dynamic cancer drivers from HER2time(SC).csv")
 
 DCD.VIMtime_SC <- findDCD(GeneExpression = GSE75688_TPM_tumor
                            , pathCovariate = "VIM"
@@ -46,7 +50,7 @@ DCD.VIMtime_SC <- findDCD(GeneExpression = GSE75688_TPM_tumor
 # Only samples from primary tumor were downloaded
 # Included in DynamicCancerDriver package as TCGA_BRCA_TP_NormCounts.rda
 
-data("TCGA_BRCA_TP_NormCounts")
+data("TCGA_BRCA_TP_NormCounts", package = "DynamicCancerDriver")
 # remove genes with no Hugo Symbol
 genes <- AMCBGeneUtils::changeGeneId(colnames(TCGA_BRCA_TP_NormCounts)
                                      , from = "Ensembl.ID")
@@ -59,7 +63,7 @@ TCGA_BRCA <-TCGA_BRCA[,colSums(TCGA_BRCA>0)>(0.2*nrow(TCGA_BRCA))
 # For comparison purposes, we keep only samples from the same patients
 # analysed in CBNA paper
 
-data(CBNApaper.patients)
+data("CBNApaper.patients", package = "DynamicCancerDriver")
 patients <- str_sub(row.names(TCGA_BRCA), start = 1, end = 12)
 index <- patients%in%CBNApaper.patients
 TCGA_BRCA <- TCGA_BRCA[index,, drop=FALSE]
@@ -70,9 +74,11 @@ TCGA_BRCA <- TCGA_BRCA[index,, drop=FALSE]
 DCD.HER2time_Bulk <- findDCD(GeneExpression = TCGA_BRCA
                            , pathCovariate = "HER2"
                            , PPItop = 0.4
-                           , findEvent = TRUE)
+                           , findEvent = TRUE
+                           , project = "BRCA")
 
 DCD.VIMtime_Bulk <- findDCD(GeneExpression = TCGA_BRCA
                           , pathCovariate = "VIM"
                           , PPItop = 0.4
-                          , findEvent = TRUE)
+                          , findEvent = TRUE
+                          , project = "BRCA")
